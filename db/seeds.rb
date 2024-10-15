1c810f588b5852f6304e2e4af3e68fa6042d905c
Ross Byrne <ross.byrne@outlook.ie>
2024-10-15 14:40:25 +0200

    Working on parsing json data to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,41 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Decodes and cleans up scraped image url
+def process_image_url(url)
+  url = CGI.unescape(url)
+  split = url.split('https://imagesvc.meredithcorp.io/v3/mm/image?url=')
+
+  if split.size > 1
+    url = split[1]
+  end
+
+  url
+end
+
+# Load Recipes from json file and parse to hash
+recipe_file = File.read('data/recipes-en.json')
+recipe_list = JSON.parse(recipe_file)
+
+puts "Number of recipes: #{recipe_list.size}"
+puts recipe_list.first
+
+# Seed DB using find_or_create
+recipe_list.each do |recipe|
+  data = recipe.slice(
+    "title",
+    "cook_time",
+    "prep_time",
+    "category",
+    "author",
+    "image"
+  )
+
+  # decode and process url
+  process_url = process_image_url(data["image"])
+  data["image"] = process_url
+
+  test = Recipe.new(data)
+  puts test.image
+end
