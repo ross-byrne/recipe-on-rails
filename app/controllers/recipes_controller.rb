d51813dd4553c46b283db8d12a94583a7dd61d02
Ross Byrne <ross.byrne@outlook.ie>
2024-10-16 15:54:31 +0200

    Working on displayed selected recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,13 @@
 class RecipesController < ApplicationController
   def index
     @all_recipes = Recipe.includes(:ingredients).limit(100)
+
+    unless recipe_params[:selected].blank?
+      @selected = Recipe.find(recipe_params[:selected])
+    end
+  end
+
+  def recipe_params
+    params.permit(:selected)
   end
 end
