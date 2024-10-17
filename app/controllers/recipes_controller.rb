14d5dc28d4390d9ebe3af0956374c009cde5cd4b
Ross Byrne <ross.byrne@outlook.ie>
2024-10-17 11:21:01 +0200

    Can now search for recipes by title

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,28 @@
 class RecipesController < ApplicationController
   def index
-    @all_recipes = Recipe.includes(:ingredients).limit(100)
+    @all_recipes = Recipe
+      .includes(:ingredients)
+      .order(:title)
+      .limit(100)
 
     unless recipe_params[:selected].blank?
       @selected = Recipe.find(recipe_params[:selected])
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def search
+     @search_results = Recipe
+       .includes(:ingredients)
+       .where("lower(title) LIKE :query", { query: "%#{params[:search].downcase}%" })
+       .order(:title)
+       .limit(20)
+
+     respond_to do |format|
+      format.html { render layout: false, status: :ok }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get "/", to: "recipes#index"
+  # get "/", to: "recipes#index"
+  root "recipes#index"
+
+  resource :recipes, only: [ :show ] do
+    post :search
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,7 +15,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
